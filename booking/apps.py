de62eb8282fe1ac215b7b2f224a3b895dc8bd53b
goutham499 <146561979+goutham499@users.noreply.github.com>
2025-07-30 11:33:18 +0530

    Update apps.py

--- booking/apps.py
+++ booking/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'
-     def ready(self):
+    def ready(self):
         import booking.signals 
